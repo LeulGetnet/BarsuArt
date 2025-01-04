c2f6218dbaffecd4db8252251fac90de508d6920
leul getnet <leulgetnet7@gmail.com>
2025-01-04 09:31:49 -0500

    procfile and setting configuration

--- barsuart/settings.py
+++ barsuart/settings.py
@@ -98,11 +98,7 @@ WSGI_APPLICATION = 'barsuart.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {dj_database_url.config(conn_max_age=600)}
 }
 
 
@@ -142,7 +138,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Optional: Additional locations of static files
 STATICFILES_DIRS = [
